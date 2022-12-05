d7042d9346bea4021bf088630b248b0cc79e844b
Samuel Luis dos Santos <mucasantos@gmail.com>
2022-12-04 23:04:44 -0300

    first program

--- app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
+++ app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
@@ -2,10 +2,62 @@ package com.example.alcoolougasolina
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun calcValor(view: View){
+        val precoAlcool = findViewById<EditText>(R.id.editPrecoAlcool)
+        val precoGasolina = findViewById<EditText>(R.id.editPrecoGasolina)
+        val textResultado = findViewById<TextView>(R.id.textResultado)
+
+        val validaCampos = validarCampos(precoAlcool.text.toString(), precoGasolina.text.toString())
+
+        if (validaCampos){
+
+            calcularMelhorPreco(precoAlcool.text.toString(), precoGasolina.text.toString())
+        }else {
+            textResultado.text = "Por favor digite um valor"
+
+        }
+
+    }
+
+    fun calcularMelhorPreco(precoAlcool: String, precoGasolina: String){
+
+        val valAlcool = precoAlcool.toDouble()
+        val valGasolina = precoGasolina.toDouble()
+        val textResultado = findViewById<TextView>(R.id.textResultado)
+
+
+        val resultadoPreco = valAlcool / valGasolina
+
+        if(resultadoPreco >= 0.7){
+
+            textResultado.text = "Melhor usar gasolina"
+
+        }else {
+            textResultado.text = "Melhor usar alcool"
+
+        }
+    }
+
+    fun validarCampos(precoAlcool: String, precoGasolina: String) : Boolean{
+
+        var camposValidados: Boolean = true;
+
+        if( precoAlcool == null || precoAlcool ==""){
+            camposValidados = false
+        }else  if( precoGasolina == null || precoGasolina ==""){
+            camposValidados = false
+        }
+
+        return camposValidados
     }
 }
